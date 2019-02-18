94b6c951010b042a8f4ddd7bd71644dbe6c7e940
Bocai Cen <bocai.cen@gmail.com>
2019-02-18 13:09:03 -0500

    handle sse

--- serverd.go
+++ serverd.go
@@ -1,35 +1,67 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/r3labs/sse"
 	"gopkg.in/macaron.v1"
 )
 
-func handleVerification(ctx *macaron.Context) {
+func handleFacebook(ctx *macaron.Context) bool {
 	modes := ctx.QueryStrings("hub.mode")
 	if len(modes) != 1 || modes[0] != "subscribe" {
-		ctx.Status(400)
-		return
+		return false
 	}
 
 	challenges := ctx.QueryStrings("hub.challenge")
 	if len(challenges) != 1 {
 		ctx.Status(400)
-		return
+		return true
 	}
 
 	channel := ctx.Params(":channel")
 	tokens := ctx.QueryStrings("hub.verify_token")
 	if len(tokens) != 1 || tokens[0] != channel {
 		ctx.Status(400)
-		return
+		return true
 	}
 
 	ctx.PlainText(200, []byte(challenges[0]))
+	return true
+}
+
+func handleWebhookConnect(ctx *macaron.Context) {
+	if handled := handleFacebook(ctx); handled {
+		return
+	}
+	channel := ctx.Params(":channel")
+	ctx.Redirect(fmt.Sprintf("/events?stream=%s", channel))
+}
+
+func handleWebhookForward(ctx *macaron.Context, events *sse.Server) {
+	events.Publish(ctx.Params(":channel"), &sse.Event{
+		Data: []byte("ping"),
+	})
+	ctx.Status(200)
 }
 
 func main() {
+	e := sse.New()
+	e.AutoStream = true
+	e.AutoReplay = false
+
 	m := macaron.Classic()
+	m.Map(e)
 	m.Use(macaron.Renderer())
-	m.Get("/webhook/:channel", handleVerification)
-	m.Run()
+	m.Get("/webhook/:channel", handleWebhookConnect)
+	m.Post("/webhook/:channel", handleWebhookForward)
+
+	host, port := macaron.GetDefaultListenInfo()
+	addr := host + ":" + strconv.Itoa(port)
+	mux := http.NewServeMux()
+	mux.Handle("/", m)
+	mux.HandleFunc("/events", e.HTTPHandler)
+	fmt.Println(http.ListenAndServe(addr, mux))
 }
